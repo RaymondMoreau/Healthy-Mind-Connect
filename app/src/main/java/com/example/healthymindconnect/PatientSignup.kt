93a3af75051463724f94504e39044f5f109c823e
Ashton-Hagar <ahaga081@uottawa.ca>
2022-06-19 22:13:40 -0400

    patient sign up fix

--- app/src/main/java/com/example/healthymindconnect/PatientSignup.kt
+++ app/src/main/java/com/example/healthymindconnect/PatientSignup.kt
@@ -1,13 +1,20 @@
 package com.example.healthymindconnect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PatientSignup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.patient_signup_page)
 
+        val patientSignup: Button = findViewById(R.id.PatientSignUp)
 
+        patientSignup.setOnClickListener{
+            var intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
     }
 }
